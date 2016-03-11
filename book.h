f55580d6806aeb88189964efb5795814a075bce7
kjwill555 <kjwill555@gmail.com>
2016-03-11 15:30:30 -0800

    Update book.h
    changed isOlder() to a friend function

--- book.h
+++ book.h
@@ -7,52 +7,53 @@
 using namespace std;
 
 class Book {
-	private:
-	protected:
-		string title;
-		string isbn;
-		string author;
-		string publisher;
-		string dateAdded;
-		double retail;
-		double wholesale;
-		int quantity;
-	public:
-		Book() {
-			title = "MISSING";
-			isbn = "MISSING";
-			author = "MISSING";
-			publisher = "MISSING";
-			dateAdded = "MISSING";
-			retail = -1.0;
-			wholesale = -1.0;
-			quantity = -1;
-		}
-		Book (string, // title
-			  string, // isbn
-			  string, // author
-			  string, // publisher
-			  string, // dateAdded
-			  double, // retail
-			  double, // wholesale
-			  int); // quantity
-		void setTitle(string);
-		void setISBN(string);
-		void setAuthor(string);
-		void setPublisher(string);
-		void setDateAdded(string);
-		void setRetail(double);
-		void setWholeSale(double);
-		void setQuantity(int);
-		string getTitle();
-		string getISBN();
-		string getAuthor();
-		string getPublisher();
-		string getDateAdded();
-		double getRetail();
-		double getWholeSale();
-		int getQuantity();
-		bool isOlder(string);
+private:
+protected:
+	string title;
+	string isbn;
+	string author;
+	string publisher;
+	string dateAdded;
+	double retail;
+	double wholesale;
+	int quantity;
+public:
+	Book() {
+		title = "MISSING";
+		isbn = "MISSING";
+		author = "MISSING";
+		publisher = "MISSING";
+		dateAdded = "MISSING";
+		retail = -1.0;
+		wholesale = -1.0;
+		quantity = -1;
+	}
+	Book(string, // title
+		string, // isbn
+		string, // author
+		string, // publisher
+		string, // dateAdded
+		double, // retail
+		double, // wholesale
+		int); // quantity
+	void setTitle(string);
+	void setISBN(string);
+	void setAuthor(string);
+	void setPublisher(string);
+	void setDateAdded(string);
+	void setRetail(double);
+	void setWholeSale(double);
+	void setQuantity(int);
+	string getTitle();
+	string getISBN();
+	string getAuthor();
+	string getPublisher();
+	string getDateAdded();
+	double getRetail();
+	double getWholeSale();
+	int getQuantity();
+	//bool isOlder(string);
+	friend bool isOlder(const Book&, const Book&);
 };
 
 #endif
